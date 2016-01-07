1cf76cf77bd1667317b9a2d22626957f17593cf1
Alexander Juarez <tall@fb.com>
2016-01-07 09:27:15 -0800

    initial setup

---
+++ app.js
@@ -0,0 +1,36 @@
+var config = require('./config');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+app.locals.PORT = process.env.PORT || 3000;
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//setup routes
+routes(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+config(app);
+
+module.exports = app;
