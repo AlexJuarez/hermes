13234cc8af4628b73743ec59d0f8e869605ab4d1
Alexander Juarez <tall@fb.com>
2016-01-15 11:59:48 -0800

    fixed url bug

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,19 +1,24 @@
 
 describe('Login to facebook.com', () => {
   it('should login as me', (done) => {
-    browser
-      .get('https://www.facebook.com');
-    browser
-      .getTitle()
-      .then((title) => {
-        expect(title).toEqual('Facebook - Log In or Sign Up');
-        done();
-      });
-    browser
-      .findElement(browser.select('#email'))
-      .sendKeys('***REMOVED***');
-    browser
-      .findElement(browser.select('#pass'))
-      .sendKeys('***REMOVED***');
+    Promise.all([
+      browser
+        .get('https://www.facebook.com/'),
+      browser
+        .getTitle()
+        .then((title) => {
+          expect(title).toEqual('Facebook - Log In or Sign Up');
+          done();
+        }),
+      browser
+        .findElement(browser.select('#email'))
+        .sendKeys('***REMOVED***'),
+      browser
+        .findElement(browser.select('#pass'))
+        .sendKeys('***REMOVED***'),
+      browser
+        .findElement(browser.select('#loginbutton > input'))
+        .sendKeys(browser.keys.ENTER)
+    ]).finally(done);
   });
 });
