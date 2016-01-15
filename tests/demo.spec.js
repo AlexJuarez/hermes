d5ed065223ed3362d4a3612bfe39a428185e589e
Alexander Juarez <tall@fb.com>
2016-01-15 14:37:52 -0800

    need to debug test failures

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,24 +1,35 @@
 
 describe('Login to facebook.com', () => {
   it('should login as me', (done) => {
-    Promise.all([
-      browser
-        .get('https://www.facebook.com/'),
-      browser
-        .getTitle()
-        .then((title) => {
-          expect(title).toEqual('Facebook - Log In or Sign Up');
-          done();
-        }),
-      browser
-        .findElement(browser.select('#email'))
-        .sendKeys('***REMOVED***'),
-      browser
-        .findElement(browser.select('#pass'))
-        .sendKeys('***REMOVED***'),
-      browser
-        .findElement(browser.select('#loginbutton > input'))
-        .sendKeys(browser.keys.ENTER)
-    ]).finally(done);
+    browser
+      .manage()
+      .window()
+      .maximize();
+    browser
+      .get('https://www.facebook.com/');
+    browser
+      .getTitle()
+      .then((title) => {
+        expect(title).toEqual('Facebook - Log In or Sign Up');
+        browser
+          .findElement(browser.select('#email'))
+          .sendKeys('***REMOVED***');
+        browser
+          .findElement(browser.select('#pass'))
+          .sendKeys('***REMOVED***');
+        browser
+          .findElement(browser.select('#loginbutton > input'))
+          .sendKeys(browser.keys.ENTER);
+        browser
+          .get('https://www.facebook.com/?_rdr=p')
+          .then(done);
+
+        /*browser
+          .getTitle()
+          .then((title) => {
+            expect(title).toEqual('Facebook');
+          });*/
+      });
+
   });
 });
