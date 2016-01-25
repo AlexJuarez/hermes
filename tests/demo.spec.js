7cf50771531cd43a042826f4a1b1757cec5c4bbd
Alexander Juarez <tall@fb.com>
2016-01-25 14:42:52 -0800

    moved user creds into file

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,4 +1,6 @@
 
+var creds = require('.creds.json');
+
 describe('Login to facebook.com', () => {
   it('should login as me', (done) => {
     browser
@@ -13,10 +15,10 @@ describe('Login to facebook.com', () => {
         expect(title).toEqual('Facebook - Log In or Sign Up');
         browser
           .findElement(browser.select('#email'))
-          .sendKeys('***REMOVED***');
+          .sendKeys(creds.username);
         browser
           .findElement(browser.select('#pass'))
-          .sendKeys('***REMOVED***');
+          .sendKeys(creds.password);
         browser
           .findElement(browser.select('#loginbutton > input'))
           .sendKeys(browser.keys.ENTER);
