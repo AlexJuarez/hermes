f8f235ce487b6fc01686eed4667cea5098b13f1e
Alexander Juarez <tall@fb.com>
2016-02-02 16:51:11 -0800

    updated conf for broswer runs

--- hermes.conf.js
+++ hermes.conf.js
@@ -1,24 +1,23 @@
 // hermes.conf.js
 module.exports = function(config) {
   config.set({
-    autoWatch: true,
-    plugins: ['jasmine'],
+    autoWatch: false,
+    plugins: ['jasmine', 'chrome'],
     reporters: ['progress'],
     files: [
-      'lib/**/*.test.js'
+      'tests/**/*.spec.js'
     ],
     exclude: [
     ],
-    /*proxy: {
+    proxy: {
       httpPort: 3000,
       httpsPort: 3001,
       domain: 'localhost',
       start: true
-    },*/
+    },
     colors: true,
     port: 9887,
-    iterations: 1,
-    logLevel: config.LOG_DEBUG,
-    browsers: []
+    iterations: 5,
+    logLevel: config.LOG_DEBUG
   });
 };
