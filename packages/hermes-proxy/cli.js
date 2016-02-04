832695b08019d4c9c0e164cd26d0c08f364acee3
Alexander Juarez <tall@fb.com>
2016-02-04 12:44:55 -0800

    updated the proxy cli, spliting the run time commands into start and managed

--- packages/hermes-proxy/cli.js
+++ packages/hermes-proxy/cli.js
@@ -5,13 +5,34 @@
 let program = require('commander');
 const Client = require('./client');
 const pkg = require('./package.json');
-
+const Proxy = require('./server');
 
 function Run() {
   program
-    .version(pkg.version)
-    .usage('[options]')
-    .option('--protocol [value]',
+    .version(pkg.version);
+
+  program
+    .command('start')
+    .option('-p, --http-port <n>',
+      'http port for the proxy',
+      parseInt)
+    .option('-s, --https-port <n>',
+      'https port for the proxy',
+      parseInt)
+    .action((options) => {
+      const config = {
+        httpPort: options.httpPort,
+        httpsPort: options.httpsPort
+      };
+
+      const proxy = new Proxy(config);
+      proxy.startup();
+    });
+
+  program
+    .command('managed')
+    .description('runs a proxy client with a managing server')
+    .option('-P, --protocol [value]',
       'protocol for the proxy manager, defaults to http',
       /^(http|https)$/i)
     .option('-p, --port <n>',
@@ -21,16 +42,20 @@ function Run() {
       'domain for the proxy manager, defaults to localhost')
     .option('--no-retry',
       'do not retry if the connection fails')
+    .action((options) => {
+      const config = {
+        port: options.port,
+        protocol: options.protocol,
+        domain: options.domain,
+        retry: options.retry
+      };
+
+      new Client(config);
+    });
+
+  program
     .parse(process.argv);
 
-  const config = {
-    protocol: program.protocol,
-    hostname: program.domain,
-    port: program.port,
-    retry: program.retry
-  };
-
-  new Client(config);
 }
 
 module.exports.Run = Run;
